bfce1091c0e340f218190adfd5a9ad2066dc4728
xzp <3094997376@qq.com>
2022-05-06 14:13:30 +0800

    “init”

---
+++ Assets/Scripts/TestAndroid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestAndroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        //AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        //string message = "this is my title";
+        //string body = "this is my content";
+        //jo.Call("ShareText", message, body);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2f-150, Screen.height / 2f, 300, 100), "Android call Unity"))
+        {
+            using (AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                using (AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+
+                    jo.Call("UnityCallAndroid");
+                }
+            }
+        }
+
+
+    }
+
+
+    public Text Text;
+    public void UnityMethod(string str)
+    {
+        Debug.Log("UnityMethod被调用，参数：" + str);
+        Text.text = str;
+    }
+}
